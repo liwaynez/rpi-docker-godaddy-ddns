50d60b8337b6a87bd6bdaa875b1a4f71d9ecc67b
liwaynez <liwaynesaved@gmail.com>
2024-02-03 19:34:57 -0600

    removed jupyter

--- src/app.py
+++ src/app.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+<<<<<<< HEAD
   return 'Welcome to the Toonchs Raspberry Pi web application!'
+=======
+  return 'Welcome to da the Raspberry Pi web application!'
+>>>>>>> 82f7f68 (removed jupyter)
