d5d867b227a1954f1b26f5345d6c4f3c1b029a93
liwaynez <liwaynesaved@gmail.com>
2024-02-03 20:03:39 -0600

    kick off job

--- src/app.py
+++ src/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'Welcome to the Toonch\'s Raspberry Pi web application!'
+  return 'Welcome to the Toonch\'s Raspberry Pi web application Container!'
 =======
   return 'Welcome to da the Raspberry Pi web application!'
