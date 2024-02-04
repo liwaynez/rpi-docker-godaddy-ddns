44b28765f5cb4c1a3f7dad0ada86aa72125423c7
liwaynez <liwaynesaved@gmail.com>
2024-02-03 19:42:00 -0600

    added new metrics to the Dockerfile

--- src/app.py
+++ src/app.py
@@ -4,8 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-<<<<<<< HEAD
   return 'Welcome to the Toonchs Raspberry Pi web application!'
 =======
   return 'Welcome to da the Raspberry Pi web application!'
->>>>>>> 82f7f68 (removed jupyter)
